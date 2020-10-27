08a9f5ba3601fe5466ac8b5afa46ed9fc262e674
Abhijit Adhikary <abhi.drmc@gmail.com>
2020-10-27 22:24:12 +1100

    resolution updated in inference

--- inference.py
+++ inference.py
@@ -27,14 +27,18 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.999))
 model, optimizer, epoch_start, loss_train_best = load_pretrained_model(load_checkpoint, checkpoint_name, checkpoint_path, model, optimizer, epoch_start=0, loss_train_best=0)
 model.eval()
 # To capture video from webcam.
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+cap.set(3, 1920)
+cap.set(4, 1080)
+cap.set(5, 60)
 # To use a video file as input
 # cap = cv2.VideoCapture('filename.mp4')
 
 while True:
     # Read the frame
     _, input_image = cap.read()
-    input_image = input_image[:, 80:80 + 480]
+    height_input, width_input, num_channels = input_image.shape
+    input_image = input_image[:, width_input//2-height_input//2:width_input//2-height_input//2+height_input]
 
     resized_image = cv2.resize(input_image, (height, width), interpolation=cv2.INTER_AREA)
     resized_image_tensor = transforms.ToTensor()(resized_image).unsqueeze(0).to(device)
@@ -86,17 +90,18 @@ while True:
     # cv2.imshow('img', image_overlaid)
     # cv2.imshow('img', resized_image)
     # Stop if escape key is pressed
-
+    joints_X *= (height_input / image_size)
+    joints_Y *= (height_input / image_size)
     for i in range((len(joints_X) - 1)):
         if i == 9 or i == 5:
             continue
-        cv2.putText(resized_image, "{}".format(i), (joints_X[i], joints_Y[i]), cv2.FONT_HERSHEY_PLAIN, fontScale=0.7, color=(255, 255, 255))
+        cv2.putText(input_image, "{}".format(i), (joints_X[i], joints_Y[i]), cv2.FONT_HERSHEY_PLAIN, fontScale=0.7, color=(255, 255, 255))
         start_point = (joints_X[i], joints_Y[i])
         end_point = (joints_X[i + 1], joints_Y[i + 1])
-        cv2.line(resized_image, start_point, end_point, color=(255, 0, 0), thickness=2)
+        cv2.line(input_image, start_point, end_point, color=(0, 0, 255), thickness=5)
 
-    resized_image = cv2.resize(resized_image, (1080, 1080), interpolation=cv2.INTER_AREA)
-    cv2.imshow('img', resized_image)
+    # resized_image = cv2.resize(resized_image, (1080, 1080), interpolation=cv2.INTER_AREA)
+    cv2.imshow('img', input_image)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
